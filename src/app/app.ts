cdfbf4c72f300ca23cd61e39fcf9b9ccdb980dd2
josemaba22 <josemaba_22@hotmail.com>
2026-06-09 23:29:16 -0600

    fix(button): invoke signals correctly in disabled binding for button examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Catalog } from './components/products/catalog/catalog';
 import { Example } from '../app/shared/button/example/example';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
